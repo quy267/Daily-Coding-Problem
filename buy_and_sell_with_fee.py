8f31e9d8830d3b8145f9086a8fd72430782e4580
quynt <quynt6803@seta-international.vn>
2020-11-17 20:28:01 +0700

    This problem was asked by Affirm.

---
+++ buy_and_sell_with_fee.py
@@ -0,0 +1,24 @@
+"""
+This problem was asked by Affirm.
+
+Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock. You're also given a number fee that represents a transaction fee for each buy and sell transaction.
+
+You must buy before you can sell the stock, but you can make as many transactions as you like.
+
+For example, given [1, 3, 2, 8, 4, 10] and fee = 2, you should return 9, since you could buy the stock at 1 dollar, and sell at 8 dollars, and then buy it at 4 dollars and sell it at 10 dollars. Since we did two transactions, there is a 4 dollar fee, so we have 7 + 6 = 13 profit minus 4 dollars of fees.
+"""
+
+
+def buy_and_sell_with_fee(arr, fee):
+    current_max_profit = 0
+    hold = -arr[0]
+
+    for price in arr[1:]:
+        current_max_profit = max(hold, hold + price - fee)
+        hold = max(hold, current_max_profit - price)
+
+    return current_max_profit
+
+
+if __name__ == '__main__':
+    print(buy_and_sell_with_fee(arr=[1, 3, 2, 8, 4, 10], fee=2))
